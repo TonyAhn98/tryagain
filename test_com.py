ce76a33d7f0d4aab2529b8d02d93d8103a6f19bb
TonyAhn98 <ash981222@gmail.com>
2023-05-15 16:08:11 +0900

    update2

--- test_com.py
+++ test_com.py
@@ -1,21 +1,15 @@
 import os
 import sys
 
-# Define the size of the grid
 GRID_SIZE = 20
 
-# Define the position of the icon
 icon_position = 0
 
-# Create the grid
 grid = [["0" for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 
-# Loop until the user quits
 while True:
-    # Clear the screen
     os.system("clear")
 
-    # Update the grid with the current position of the icon
     for row in range(GRID_SIZE):
         for col in range(GRID_SIZE):
             if col == icon_position:
@@ -23,11 +17,9 @@ while True:
             else:
                 grid[row][col] = "0"
 
-    # Print the grid to the screen
     for row in grid:
         print(" ".join(row))
 
-    # Wait for the user to press Enter
     print("\nPress Enter to move the icon to the right...", end="")
     sys.stdout.flush()
     input()
